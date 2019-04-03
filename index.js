892ee1fbcd7ef50d7f135a539e7d1fc478af4105
Connor Bode <connor.bode@gmail.com>
2019-04-03 15:29:20 -0400

    update readme, handle ios errors

--- index.js
+++ index.js
@@ -6,6 +6,35 @@ let callbackUrl
 const errors = {
 	CANNOT_OPEN_SQUARE: 'CANNOT_OPEN_SQUARE',
 	UNKNOWN_IOS_ERROR: 'UNKNOWN_IOS_ERROR',
+	UNKNOWN_ANDROID_ERROR: 'UNKNOWN_ANDROID_ERROR',
+	PAYMENT_CANCELLED: 'PAYMENT_CANCELLED',
+	NOT_LOGGED_IN: 'NOT_LOGGED_IN',
+	USER_NOT_ACTIVE: 'USER_NOT_ACTIVE',
+	AMOUNT_TOO_SMALL: 'AMOUNT_TOO_SMALL',
+	AMOUNT_TOO_LARGE: 'AMOUNT_TOO_LARGE',
+	INVALID_TENDER_TYPE: 'INVALID_TENDER_TYPE',
+	UNSUPPORTED_TENDER_TYPE: 'UNSUPPORTED_TENDER_TYPE',
+	NO_NETWORK_CONNECTION: 'NO_NETWORK_CONNECTION',
+	TRANSACTION_ALREADY_IN_PROGRESS: 'TRANSACTION_ALREADY_IN_PROGRESS',
+}
+
+const iosErrors = {
+	payment_canceled: errors.PAYMENT_CANCELLED,
+	not_logged_in: errors.NOT_LOGGED_IN,
+	user_not_active: errors.USER_NOT_ACTIVE,
+	amount_too_small: errors.AMOUNT_TOO_SMALL,
+	amount_too_large: errors.AMOUNT_TOO_LARGE,
+	invalid_tender_type: errors.INVALID_TENDER_TYPE,
+	unsupported_tender_type: errors.UNSUPPORTED_TENDER_TYPE,
+	no_network_connection: errors.NO_NETWORK_CONNECTION,
+}
+
+const androidErrors = {
+	TRANSACTION_CANCELED: errors.PAYMENT_CANCELLED,
+	USER_NOT_LOGGED_IN: errors.NOT_LOGGED_IN,
+	USER_NOT_ACTIVATED: errors.USER_NOT_ACTIVE,
+	NO_NETWORK: errors.NO_NETWORK_CONNECTION,
+	TRANSACTION_ALREADY_IN_PROGRESS: errors.TRANSACTION_ALREADY_IN_PROGRESS, 
 }
 
 const RNSquarePos = {
@@ -28,7 +57,7 @@ const RNSquarePos = {
 					}
 				}
 
-				DeviceEventEmitter.addListener('RNSquarePOSResponse', handleResponse);
+				DeviceEventEmitter.addListener('RNSquarePOSReponse', handleResponse);
 			} else if (Platform.OS === 'ios') {
 				SquarePOS.startTransaction(amount, currency, options, callbackUrl, (errorCode, errorDescription) => {
 					switch (errorCode) {
@@ -55,9 +84,16 @@ const RNSquarePos = {
 						const data = JSON.parse(decodeURIComponent(url.split('?data=')[1]))
 
 						if (data.error_code) {
-							return reject({
-								errorCode: data.error_code.toUpperCase()
-							})
+							if (iosErrors[data.error_code]) {
+								return reject({
+									errorCode: iosErrors[data.error_code]
+								})
+							} else {
+								return reject({
+									errorCode: errors.UNKNOWN_IOS_ERROR,
+									originalCode: data.error_code
+								})
+							}
 						} else {
 							return resolve({
 								transactionId: data.transaction_id,
